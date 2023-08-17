cdde33aa523d7bc2d376c6a5630e26b6f8c46aee
vahram87 <vahram.ts@gmail.com>
2023-08-17 08:15:31 +0400

    Did changes

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,38 @@
+class Admin::UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :find_user, only: %i[show edit update destroy]
+
+  def index
+    @users = User.page(params[:page])
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      flash[:success] = t('admin.users.update')
+      redirect_to admin_user_path(@user)
+    else
+      flash[:danger] = t('admin.users.errors.error')
+      render 'edit'
+    end
+  end
+
+  private
+
+  def find_user
+    @user = User.find_by_id(params[:id])
+    return if @user
+
+    flash[:danger] = t('admin.users.errors.not_found')
+    redirect_to admin_root_path
+  end
+
+  def user_params
+    params.require(:user).permit(:status)
+  end
+end
