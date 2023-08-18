d1f0e09aaf091a2bcea475914ee56dd734100943
vahram87 <vahram.ts@gmail.com>
2023-08-18 14:23:19 +0400

    Fixed menus

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,2 +1,31 @@
 class Admin::PostsController < ApplicationController
+  def index
+    @posts = Post.page(params[:page])
+  end
+
+   def create
+    @post = Post.new(post_params)
+    if @post.save
+      flash[:success] = t('admin.posts.create')
+      redirect_to admin_post_path(@post)
+    else
+      flash[:danger] = t('admin.posts.error')
+      redirect_to new_admin_post_path
+    end
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to admin_posts_path
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :contant)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root 'home#index'
 
   namespace :admin do
+    root 'home#index'
     resources :posts
     resources :users
   end
